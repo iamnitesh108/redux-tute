260649b554d43545d44521d5bba32357790faf1d
iamnitesh108 <iamnitesh108@gmail.com>
2023-09-22 19:37:44 +0530

    action with payload implemented

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-//store 
-const store = createStore(reducer);
+//store
+const store = createStore(reducer, applyMiddleware(logger.default));
 
-const history = []
+const history = [];
 
 // Reducer returns state
-function reducer(state={amount: 1}, action) {
-    if(action.type === 'increment') {
-        return {amount: state.amount+1}; // not state.amount+1
-    }
-    return state;
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    return { amount: state.amount + 1 }; // not state.amount+1
+  }
+  if (action.type === "decrement") {
+    return { amount: state.amount - 1 }; // not state.amount+1
+  }
+  if (action.type === "incrementByAmount") {
+    return { amount: state.amount + action.payload }; // not state.amount+1
+  }
+  return state;
 }
 
-// Global state
-store.subscribe(() => {
-    history.push(store.getState())
-    console.log(history)
-})
+
 
 //Dispatch Event
 setInterval(() => {
-    store.dispatch({type: 'increment'})
-},3000)
+  store.dispatch({ type: "incrementByAmount", payload: 10 });
+}, 1000);
