93855becc910ad7fd8afde9cc059e9b42eb71aac
iamnitesh108 <iamnitesh108@gmail.com>
2023-09-22 20:20:26 +0530

    Action name constant added

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
+//Action name constant
+const inc = "increment";
+const dec = "decrement";
+const incByAmt = "incrementByAmount"
+
 //store
 const store = createStore(reducer, applyMiddleware(logger.default));
 
 const history = [];
 
+
 // Reducer returns state
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === "increment") {
+  if (action.type === inc) {
     return { amount: state.amount + 1 }; // not state.amount+1
   }
-  if (action.type === "decrement") {
+  if (action.type === dec) {
     return { amount: state.amount - 1 }; // not state.amount+1
   }
-  if (action.type === "incrementByAmount") {
+  if (action.type === incByAmt) {
     return { amount: state.amount + action.payload }; // not state.amount+1
   }
   return state;
@@ -22,13 +28,13 @@ function reducer(state = { amount: 1 }, action) {
 
 // Action Creators
 function increment() {
-    return {type: 'increment'}
+    return {type: inc}
 }
 function decrement() {
-    return {type: 'decrement'}
+    return {type: dec}
 }
 function incrementByAmount(value) {
-    return {type: 'incrementByAmount', payload: value}
+    return {type: incByAmt, payload: value}
 }
 //Dispatch Event
 setInterval(() => {
