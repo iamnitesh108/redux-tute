b0e6f6b43b9d5e1ee95df11cac4f5beab52f9c0c
iamnitesh108 <iamnitesh108@gmail.com>
2023-09-23 13:39:43 +0530

    Async dispatch is handled with react-thunk

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import axios from "axios";
+import thunk from "redux-thunk";
 
 //Action name constant
+const init = "init";
 const inc = "increment";
 const dec = "decrement";
-const incByAmt = "incrementByAmount"
+const incByAmt = "incrementByAmount";
 
 //store
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(reducer, applyMiddleware(logger.default, thunk.default));
 
 const history = [];
 
-
 // Reducer returns state
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === inc) {
-    return { amount: state.amount + 1 }; // not state.amount+1
+  switch(action.type) {
+    case init: 
+      return {amount: action.payload};
+
+    case inc: 
+      return { amount: state.amount + 1 };
+    
+    case dec:
+      return { amount: state.amount - 1 };
+    
+    case incByAmt: 
+      return { amount: state.amount + action.payload }; // not state.amount+1
+    
+    default:
+      return state;
   }
-  if (action.type === dec) {
-    return { amount: state.amount - 1 }; // not state.amount+1
-  }
-  if (action.type === incByAmt) {
-    return { amount: state.amount + action.payload }; // not state.amount+1
-  }
-  return state;
+  
 }
+// Async API call
+// async function getUser() {
+//   const { data } = await axios.get("http://localhost:3000/accounts/1");
+// }
+
 
 // Action Creators
+async function initUser(dispatch, getState) {
+  const { data } = await axios.get("http://localhost:3000/account/2");
+
+  dispatch( { type: init, payload: data.amount } );
+}
 function increment() {
-    return {type: inc}
+  return { type: inc };
 }
 function decrement() {
-    return {type: dec}
+  return { type: dec };
 }
 function incrementByAmount(value) {
-    return {type: incByAmt, payload: value}
+  return { type: incByAmt, payload: value };
 }
+
 //Dispatch Event
 setInterval(() => {
-  store.dispatch(incrementByAmount(100));
+  store.dispatch(initUser);
 }, 1000);
+
